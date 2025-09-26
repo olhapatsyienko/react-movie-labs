eaa88b0961fe40342f63bb37d4804fd8275727df
olhapatsyienko <20107984@mail.wit.ie>
2025-09-26 18:52:32 +0100

    movie card component created

--- movies/src/pages/homePage.jsx
+++ movies/src/pages/homePage.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Grid from "@mui/material/Grid";
+import MovieCard from '../components/index.jsx'
 
 const HomePage = (props) => {
   const movies = props.movies;
@@ -8,6 +9,9 @@ const HomePage = (props) => {
   <Grid container>
       <Grid item xs={12}>
           <h1> HomePage </h1>
+      </Grid>
+      <Grid item xs={3}>
+        <MovieCard movie={movies[0]} />
       </Grid>
     </Grid>
   );
