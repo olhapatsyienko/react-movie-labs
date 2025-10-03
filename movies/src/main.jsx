371f1649528a16cb5ad96461fb12c754cc25fdf0
olhapatsyienko <20107984@mail.wit.ie>
2025-10-03 12:42:03 +0100

    Movie Details Page rendering

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import HomePage from "./pages/homePage.jsx";
+import MovieDetailsPage from './pages/movieDetailsPage'
+
 
 const sample = {
   adult: false,
@@ -83,13 +85,22 @@ const sample = {
   vote_count: 9692,
 };
 
+
 const movies = [sample, sample, sample, sample, sample, sample, sample];
+
+const images = [
+  "/kOVEVeg59E0wsnXmF9nrh6OmWII.jpg",
+  "/v1QQKq8M0fWxMgSdGOX1aCv8qMB.jpg",
+  "/2iGN0aKHJYD0xQydlfuCUAcgNbO.jpg",
+  "/rjBwhsOzHKUw2NIOrE7aMqjfe6s.jpg",
+]
 
 const App = () => {
   return (
-      <HomePage movies={movies} />
-  );
+      <MovieDetailsPage movie={sample} images={images} />
+      );
 };
+
 
 const rootElement = createRoot( document.getElementById("root") )
 rootElement.render(<App />);
