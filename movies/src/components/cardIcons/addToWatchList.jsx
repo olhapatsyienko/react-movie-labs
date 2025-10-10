03c7131efab80f19c574a82ebb745f41beb725bd
olhapatsyienko <20107984@mail.wit.ie>
2025-10-10 18:08:10 +0100

    Tagging upcoming movies as must-watch - completed Movie Labs!

--- movies/src/components/cardIcons/addToWatchList.jsx
+++ movies/src/components/cardIcons/addToWatchList.jsx
@@ -1,16 +1,18 @@
-import React from "react";
+import React, { useContext } from "react";
 import IconButton from "@mui/material/IconButton";
 import PlaylistAddIcon from "@mui/icons-material/PlaylistAdd";
+import { MoviesContext } from "../../contexts/moviesContext";
 
 const AddToWatchListIcon = ({ movie }) => {
+  const context = useContext(MoviesContext);
+
   const handleAddToWatchList = (e) => {
     e.preventDefault();
-    // TODO: Implement add to watch list functionality
-    console.log('Add to watch list clicked for movie:', movie.title);
+    context.addToMustWatch(movie);
   };
 
   return (
-    <IconButton aria-label="add to watch list" onClick={handleAddToWatchList}>
+    <IconButton aria-label="add to must watch list" onClick={handleAddToWatchList}>
       <PlaylistAddIcon color="primary" fontSize="large" />
     </IconButton>
   );
